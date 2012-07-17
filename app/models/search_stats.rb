1cdd796c4956f380dd1fa822565838d504670f3a
Indro De <indro.de@gmail.com>
2012-07-17 12:06:13 +0200

    Add documentation

--- app/models/search_stats.rb
+++ app/models/search_stats.rb
@@ -1,31 +1,61 @@
 class SearchStats
 
-  # Public: Add/increment a counter for search terms stored in Redis
-  #
-  # term - The search string to be added.
-  # type - The usage type, e.g. "notfound"
-  #
-  # Examples
-  #
-  #   SearchStats.set_search_usage('Justin Bieber', 'search_terms')
-  #   # => 1.0
-  #
-  # Returns the Redis record.
   class <<self
+
+    # Public: Add/increment a counter for search terms stored in Redis.
+    #
+    # term - The search string to be added.
+    # type - The usage type, e.g. :notfound.
+    #
+    # Examples
+    #
+    #   SearchStats.increment_search_term_score(:notfound, 'Justin Bieber')
+    #   # => 1.0
+    #
+    # Returns the score for the given search term.
     def increment_search_term_score(type, term)
       R.zincrby(type, 1, term.chomp.downcase)
     end
 
-    # Retrieve the number of hits for a search term for a search type
+    # Public: Retrieve the number of hits for a search term for a search type
+    #
+    # term - The search string to be retrieved.
+    # type - The usage type, e.g. :notfound.
+    #
+    # Examples
+    #
+    #   SearchStats.get_search_term_score(:notfound, 'Justin Bieber')
+    #   # => 1.0
+    #
+    # Returns the score for the given search term.
     def get_search_term_score(type, term)
       R.zscore(type, term.downcase)
     end
 
-    # Retrieve the ranked terms by type
+    # Public: Retrieve the ranked terms by type.
+    #
+    # type  - The usage type, e.g. :notfound.
+    # limit - The number of terms to be returned.
+    #
+    # Examples
+    #
+    #   SearchStats.get_top_search_terms(:notfound, 2)
+    #   # => [["justin bieber", 5.0], ["lady gaga", 3.0]]
+    #
+    # Returns an ordered array of terms with scores.
     def get_top_search_terms(type, limit)
       R.zrevrange(type, 0, limit-1, { withscores: true })
     end
 
+
+    # Public: Reset the Redis store.
+    #
+    # Examples
+    #
+    #   SearchStats.reset_all_search_stats
+    #   # => OK
+    #
+    # Returns OK.
     def reset_all_search_stats
       R.flushall
     end
