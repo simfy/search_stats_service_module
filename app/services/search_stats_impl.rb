4966b2937a8bf3118f248e5cf5a12faf847cc38a
Capony <robin.kunath@gmail.com>
2012-07-17 15:16:46 +0200

    changed class names

--- app/services/search_stats_impl.rb
+++ app/services/search_stats_impl.rb
@@ -1,22 +1,22 @@
-class SetTermImpl
+class IncrementSearchTermScoreImpl
   def self.execute
     SearchStats.increment_search_term_score(type, term)
   end
 end
 
-class GetTermImpl
+class GetSearchTermScoreImpl
   def self.execute
     SearchStats.get_search_term_score(type, term)
   end
 end
 
-class GetTopTermsImpl
+class GetTopSearchTermsImpl
   def self.execute
     SearchStats.get_top_search_terms(type, limit)
   end
 end
 
-class GetTopTermsImpl
+class ResetAllSearchStatsImpl
   def self.execute
     SearchStats.reset_all_search_stats()
   end
